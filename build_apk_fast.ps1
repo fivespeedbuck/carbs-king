8675bcfb348378addb894b3dd54f1d8f791e3981
碳水大王维护者 <carbs-king@users.noreply.github.com>
2026-07-09 22:43:49 -0400

    release: v42 target range display fix

---
+++ build_apk_fast.ps1
@@ -0,0 +1,9 @@
+﻿$ErrorActionPreference = "Stop"
+$env:PYTHONUTF8 = "1"
+$env:FLET_CLI_NO_RICH_OUTPUT = "1"
+$env:HTTP_PROXY = "http://127.0.0.1:7897"
+$env:HTTPS_PROXY = "http://127.0.0.1:7897"
+$env:NO_PROXY = "localhost,127.0.0.1,::1"
+
+Set-Location $PSScriptRoot
+flet build apk --no-rich-output
